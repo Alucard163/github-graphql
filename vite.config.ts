552ed777a44d8190ad4dc072fd64155496c92307
Alucard163 <romemix163@gmail.com>
2023-08-27 18:36:23 +0400

    vite conf changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/github-graphql/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
